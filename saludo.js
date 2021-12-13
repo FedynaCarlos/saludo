acdb3c6322b461d016f22f681566956884e276c0
juan carlos fedyna <jfedyna@outlook.com>
2021-12-13 13:12:59 -0300

    subo el primer rchivo modificado!

--- saludo.js
+++ saludo.js
@@ -1,8 +1,8 @@
 //bloque n° 1   saludo.
 let saludo = "Hello Word!";
-console.log(saludo);
+//console.log(saludo);
 //fin  bloque n° 1.
 //inicio bloque n° 2  saludo.
 let saludo = "Prueba 2 de saludo!";
 console.log(saludo);
-//fin  bloque n° 2.
+//fin  bloque n° 
